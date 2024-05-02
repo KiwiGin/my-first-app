624b788dca7ab643ca2ac18a7d807957af576ced
KiwiGin <bayeskiwigen@gmail.com>
2024-05-02 00:46:46 -0500

    Añadir ImageViewer y Button components en App.js Actualizar dependencias en package.json y package-lock.json

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
+import ImageViewer from './components/ImageViewer';
+import Button from './components/Button';
+
+const imageFile = require('./assets/images/background-image.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={imageFile} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Select from gallery" />
+        <Button label="Use photo default" />
+      </View>
+      <Text style={{ color: 'white' }}>Nya</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 60,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
